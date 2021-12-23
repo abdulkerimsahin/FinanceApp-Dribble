ba17937fdc6daa49e12dcf4616633a211705e345
Kerim Sahin <a.kerimsahin@gmail.com>
2021-12-23 22:46:51 +0300

    feat: add view extension to read device size

--- FinanceApp-Replicate/FinanceApp-Replicate/View+ReadSize.swift
+++ FinanceApp-Replicate/FinanceApp-Replicate/View+ReadSize.swift
@@ -5,4 +5,21 @@
 //  Created by Abdulkerim Sahin on 23.12.2021.
 //
 
-import Foundation
+import SwiftUI
+
+extension View {
+  func readSize(onChange: @escaping (CGSize) -> Void) -> some View {
+    background(
+      GeometryReader { geometryProxy in
+        Color.clear
+          .preference(key: SizePreferenceKey.self, value: geometryProxy.size)
+      }
+    )
+    .onPreferenceChange(SizePreferenceKey.self, perform: onChange)
+  }
+}
+
+private struct SizePreferenceKey: PreferenceKey {
+  static var defaultValue: CGSize = .zero
+  static func reduce(value: inout CGSize, nextValue: () -> CGSize) {}
+}
