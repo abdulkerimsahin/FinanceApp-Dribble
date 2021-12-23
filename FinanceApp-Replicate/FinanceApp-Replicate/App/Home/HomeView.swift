cdfd0d3b2383219f34e46fd9311a7a9b93bf275a
Kerim Sahin <a.kerimsahin@gmail.com>
2021-12-24 00:26:39 +0300

    feat: add home view template

---
+++ FinanceApp-Replicate/FinanceApp-Replicate/App/Home/HomeView.swift
@@ -0,0 +1,35 @@
+//
+//  HomeView.swift
+//  FinanceApp-Replicate
+//
+//  Created by Abdulkerim Sahin on 23.12.2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @State private var size: CGSize = .zero
+    
+    var body: some View {
+        VStack {
+            CardView(size: $size)
+                .padding()
+            
+            Spacer()
+        }
+        .background {
+            Color("Background")
+                .ignoresSafeArea()
+        }
+        .navigationBarHidden(true)
+        .readSize { size in
+            self.size = size
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
